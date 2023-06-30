afd3dd574573c000a18a98bc31271b4c9a93f942
joao pedro <jjoao.monteiro15@gmail.com>
2023-06-30 19:58:04 -0300

    feat: create repository abstract by user

---
+++ src/slices/user/repositories/UserRepository.ts
@@ -0,0 +1,23 @@
+import { User } from "@prisma/client";
+import { AddUserRepository, DeleteUserRepository, GetUserRepository, UpdateUserRepository } from "./contracts";
+
+export interface IUserRepository extends AddUserRepository, GetUserRepository, UpdateUserRepository, DeleteUserRepository {}
+
+export class UserRepository implements IUserRepository{
+  constructor(protected repository: IUserRepository) {}
+  async add(data: Omit<User, 'id'>): Promise<any> {
+    return await this.repository.add(data)
+  }
+
+  async getById(id: string): Promise<User> {
+    return await this.repository.getById(id)
+  }
+
+  async update(id: string, data: Partial<User>): Promise<any> {
+    return await this.repository.update(id, data)
+  }
+
+  async delete(id: string): Promise<any> {
+    return await this.repository.delete(id)
+  }
+}
---
+++ src/slices/user/repositories/contracts/index.ts
@@ -0,0 +1,4 @@
+export * from './AddUserRepository'
+export * from './GetUserRepository'
+export * from './UpdateUserRepository'
+export * from './DeleteUserRepository'
