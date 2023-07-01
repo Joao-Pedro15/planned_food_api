999b7c475ada05095f91b44f30393a72fd30971d
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-01 09:57:38 -0300

    feat: create new method by user getByEmail

--- src/application/infra/models/PrismaUser.ts
+++ src/application/infra/models/PrismaUser.ts
@@ -1,4 +1,16 @@
 import { User } from "@prisma/client";
-import { PrismaRepository } from "../PrismaRepository";
+import { Model, PrismaRepository } from "../PrismaRepository";
+import { Prisma } from "../database/prisma";
 
-export class PrismaUser extends PrismaRepository<User, string> {}
+export class PrismaUser extends PrismaRepository<User, string> {
+  constructor(protected repository: Model<User, string>) {
+    super(repository)
+  }
+
+  async getByEmail(email: string) {
+    return Prisma.user.findFirst({
+      where: { email },
+      distinct: "name"
+    })
+  }
+}
