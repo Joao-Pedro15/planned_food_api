99a14a0a083be8db88721b4d13b1a7d8383e01d3
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-12 22:45:45 -0300

    feat: implements encrypt clas in factory addUser

--- src/application/controller/user/addUser/AddUserFactory.ts
+++ src/application/controller/user/addUser/AddUserFactory.ts
@@ -1,3 +1,4 @@
+import { CryptoAdapter } from "@/adapters";
 import { PrismaUser } from "@/application/infra";
 import { Prisma } from "@/application/infra/database/prisma";
 import { UserRepository } from "@/slices/user/repositories/UserRepository";
@@ -8,7 +9,8 @@ class AddUserFactory {
   async build(data: Omit<User, 'id'>) {
     const prismaUser = new PrismaUser(Prisma.user)
     const userRepository = new UserRepository(prismaUser)
-    const user = new AddUserUseCase(userRepository)
+    const encrypt = new CryptoAdapter()
+    const user = new AddUserUseCase(userRepository, encrypt)
     return await user.execute(data)
   }
 }
