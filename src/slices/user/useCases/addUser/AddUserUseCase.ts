54d8e3a19d62cb387c0718904e3e6eac1e090e03
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-12 22:44:39 -0300

    feat: implements protocol hash password in useCase addUser

--- src/slices/user/useCases/addUser/AddUserUseCase.ts
+++ src/slices/user/useCases/addUser/AddUserUseCase.ts
@@ -1,12 +1,17 @@
 import { User } from "@prisma/client";
 import { IUserRepository } from "../../repositories/UserRepository";
+import { Encrypter } from "@/adapters/protocols";
 
 export class AddUserUseCase {
-  constructor(private userRepository: IUserRepository) {}
+  constructor(
+    private userRepository: IUserRepository,
+    private encrypter: Encrypter
+    ) {}
 
   async execute(data: Omit<User, 'id'>) {
     const isUser = await this.userRepository.getByEmail(data.email)
     if(isUser) throw new Error("email already registered")
-    return await this.userRepository.add(data)
+    const hashPassword = await this.encrypter.encrypt(data.password, 12)
+    return await this.userRepository.add({...data, password: hashPassword})
   }
 }
