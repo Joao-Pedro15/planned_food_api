202a11be9c786704b7e71fc960e3b230d36be5e8
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-14 22:01:23 -0300

    feat: add handleError in useCase addUser

--- src/slices/user/useCases/addUser/AddUserUseCase.ts
+++ src/slices/user/useCases/addUser/AddUserUseCase.ts
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client";
 import { IUserRepository } from "../../repositories/UserRepository";
 import { Encrypter } from "@/adapters/protocols";
+import { HandleError } from "@/errors/HandleError";
 
 export class AddUserUseCase {
   constructor(
@@ -9,9 +10,14 @@ export class AddUserUseCase {
     ) {}
 
   async execute(data: Omit<User, 'id'>) {
+   try {
     const isUser = await this.userRepository.getByEmail(data.email)
     if(isUser) throw new Error("email already registered")
     const hashPassword = await this.encrypter.encrypt(data.password, 12)
     return await this.userRepository.add({...data, password: hashPassword})
+   } catch (error) {
+    if(error instanceof HandleError) throw new HandleError(error.message, error.statusCode)
+    throw new HandleError('Internal server error', 500)
+   }
   }
 }
