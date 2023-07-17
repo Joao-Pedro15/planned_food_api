24f09887f6dbedfd4a5ae6aca05c70791d326329
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-16 22:24:13 -0300

    feat: create route from login

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,14 @@
 import AddUserController from "@/application/controller/user/addUser/AddUserController";
 import GetUserByIdController from "@/application/controller/user/getUserById/GetUserByIdController";
+import LoginController from "@/application/controller/user/login/LoginController";
 GetUserByIdController
 
 import { Router } from "express";
 const router = Router()
 
 router.post('/addUser', AddUserController.handle)
+
+router.post('/login', LoginController.handle)
 
 router.get('/getUser/:id', GetUserByIdController.handle)
 
