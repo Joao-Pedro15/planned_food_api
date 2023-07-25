a8dc539d39c796f73033b24c329d2bc2546eabd2
joao pedro <jjoao.monteiro15@gmail.com>
2023-07-24 23:14:01 -0300

    feat: add validation if exist food in addMeal with items meal

--- src/slices/meal/useCases/addMeal/AddMealUseCase.spec.ts
+++ src/slices/meal/useCases/addMeal/AddMealUseCase.spec.ts
@@ -4,13 +4,14 @@ import { AddMealUseCase } from "./AddMealUseCase"
 import { Meal, MealItem } from "@prisma/client"
 import { randomUUID } from "crypto"
 import { fakeFood } from "@/slices/food/useCases/getFood/GetFoodUseCase.spec"
+import { GetFoodRepository } from "@/slices/food/repositories/contracts"
 
 const id = randomUUID()
 
-const fakeMeal: Meal = {
+export const fakeMeal: Meal = {
   id,
   date: new Date(),
-  name: 'Refeição livre',
+  name: 'Café da Manhã',
   updatedAt: null,
   userCreated: id,
   createdAt: new Date(),
@@ -18,7 +19,7 @@ const fakeMeal: Meal = {
 
 }
 
-const fakeMealItem: MealItem = {
+export const fakeMealItem: MealItem = {
   id,
   foodId: fakeFood.id,
   mealId: fakeMeal.id,
@@ -29,24 +30,37 @@ const fakeMealItem: MealItem = {
 
 describe('testing useCase by addMeal',() => {
   let addMealRepository: MockProxy<AddMealRepository>
+  let getFoodRepository: MockProxy<GetFoodRepository>
   let testInstance: AddMealUseCase
   beforeAll(() => {
     addMealRepository = mock()
+    getFoodRepository = mock()
+    getFoodRepository.getById.mockResolvedValue(fakeFood)
     addMealRepository.add.mockResolvedValue(null)
   })
   beforeEach(() => {
-    testInstance = new AddMealUseCase(addMealRepository)
+    testInstance = new AddMealUseCase(
+      addMealRepository,
+      getFoodRepository
+    )
   })
 
 
   it('should successfully add Meal call in repository with items', async () => {
-    await testInstance.execute(fakeMeal, [fakeMealItem])
-    expect(addMealRepository.add).toHaveBeenCalledWith(fakeMeal, [fakeMealItem])
+    await testInstance.execute(fakeMeal, [fakeMealItem, fakeMealItem])
+    expect(addMealRepository.add).toHaveBeenCalledWith(fakeMeal, [fakeMealItem, fakeMealItem])
+    expect(getFoodRepository.getById).toHaveBeenCalledTimes(2)
   })
 
   it("should successfully add Meal call in repository NO with item", async () => {
     await testInstance.execute(fakeMeal)
     expect(addMealRepository.add).toHaveBeenCalledWith(fakeMeal, [])
+  })
+
+  it('should return not found food passing food not exist', async () => {
+    getFoodRepository.getById.mockResolvedValueOnce(null)
+    await expect(testInstance.execute(fakeMeal, [fakeMealItem]))
+    .rejects.toThrowError('not found food')
   })
 
 })
