d12255fc22e8f8246768018139d756d5884268ac
joao pedro <jjoao.monteiro15@gmail.com>
2024-01-17 21:37:02 -0300

    refactor: change logical create routes and configure express

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,13 @@
 import 'dotenv/config'
 import './config/module-alias'
-import { auth, food, meal, user } from './routes/index'
 import express from 'express'
-import cors from 'cors'
-import { Prisma } from './application/infra/database/prisma'
+import { expressConfig } from '@/config/express-config'
 
 const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
+
+expressConfig(app)
 
 app.get("/health", (request, response) => response.status(200).send('hello world'))
 
-app.get('/teste', async (_, response) => {
-  const teste = await Prisma.category.findMany()
-  return response.status(200).json(teste)
-})
-
-app.use('/user', user.default)
-app.use('/food', food.default)
-app.use('/meal', meal.default)
-app.use('/auth', auth.default)
 
 export default app
