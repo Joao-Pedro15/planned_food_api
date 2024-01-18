dd4d76b0f3da651a80390b03e127c75fbc33ea41
joao pedro <jjoao.monteiro15@gmail.com>
2024-01-17 21:34:58 -0300

    feat: remove routes not using

--- src/routes/food.ts
+++
@@ -1,8 +0,0 @@
-import GetFoodController from '@/application/controller/food/getFood/GetFoodController'
-import { Router } from 'express'
-const router = Router()
-
-router.get("/getFoods", GetFoodController.handle)
-
-
-export default router
