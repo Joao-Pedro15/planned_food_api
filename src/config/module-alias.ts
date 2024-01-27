3f733ca7dd25be493447554c96cc10aee07cd4ff
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-27 16:16:56 -0300

    chore: change alias config

--- src/config/module-alias.ts
+++ src/config/module-alias.ts
@@ -2,4 +2,4 @@ import { addAlias } from "module-alias";
 import { resolve } from "path";
 import { access } from 'fs/promises'
 
-addAlias("@", resolve(process.env.TS_NODE_ENV !== undefined ? "dist" : "dist"));
+addAlias("@", resolve(process.env.TS_NODE_ENV !== undefined ? "dist" : "src"));
