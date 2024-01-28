b5375d25ddea6b185b285a59221b21ed976bf360
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-28 15:15:31 -0300

    feat: implements auth middleware in user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,8 +2,11 @@ import { Router } from "express";
 
 import { makeGetAllUsersControllerFactory } from '@/controllers/user'
 import { adaptRoute } from "@/main/route";
+import { authMiddleware } from "@/middlewares/auth/authMiddleware";
 
 const router = Router()
+
+router.use(authMiddleware)
 
 router.get('/getAll', adaptRoute(makeGetAllUsersControllerFactory()))
 
