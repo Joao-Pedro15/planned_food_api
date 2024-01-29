0b340d35a81e4a50b32adca00eb5b7594057f01b
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-29 12:10:21 -0300

    feat: implements interfaces by prisma in repository

--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -1,8 +1,12 @@
-import { User } from "@/domain/user/User";
+import { User, NutritionGoals } from '@prisma/client'
+
+interface UserById extends User {
+  nutritionGoals: NutritionGoals[]
+}
 
 export interface UserRepository {
   getByEmail(email: string): Promise<User>
-  getById(id: string): Promise<User>
+  getById(id: string): Promise<UserById>
   add(data: User): Promise<User>
   get(): Promise<User[]>
 }
