732e2c0c376b8405fa37a6311ccb51444d428ffa
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-29 12:09:28 -0300

    feat: add nutriotional in user domain

--- src/domain/user/User.ts
+++ src/domain/user/User.ts
@@ -1,3 +1,4 @@
+import { NutritionGoals } from "../nutritionGoals/NutritionGoals"
 import { UserDTO } from "./UserDTO"
 import { randomUUID } from 'node:crypto'
 
@@ -6,10 +7,13 @@ export class User {
   public name: string
   public email: string
   public password: string
+  public nutritionalGoals: NutritionGoals[]
   constructor(data: UserDTO) {
     this.id = data.id ?? randomUUID()
     this.name = data.name
     this.email = data.email
     this.password = data.password
+    this.nutritionalGoals = data.nutriotionalGoals ?? []
+
   }
 }
--- src/domain/user/UserDTO.ts
+++ src/domain/user/UserDTO.ts
@@ -1,7 +1,10 @@
+import { NutritionGoals } from "../nutritionGoals/NutritionGoals"
+
 export interface UserDTO {
   id?: string
   name: string
-  email:string
+  email: string
   password: string
-  confirmPassword?:string
+  confirmPassword?: string
+  nutriotionalGoals?: NutritionGoals[]
 }
