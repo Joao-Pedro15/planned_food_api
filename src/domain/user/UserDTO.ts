bedb7f91ea62c65088900316bcd3dd5b8b121093
joao pedro <jjoao.monteiro15@gmail.com>
2024-01-30 20:27:06 -0300

    feat: add more data in userDTO

--- src/domain/user/UserDTO.ts
+++ src/domain/user/UserDTO.ts
@@ -1,3 +1,4 @@
+import { activyList } from "@/usecases/baseMetabolicRate/BaseMetabolicRate"
 import { NutritionGoals } from "../nutritionGoals/NutritionGoals"
 
 export interface UserDTO {
@@ -5,6 +6,12 @@ export interface UserDTO {
   name: string
   email: string
   password: string
+  height: number
+  weight: number
+  age: number
+  gender: 'male' | 'female'
   confirmPassword?: string
+  activity?: keyof typeof activyList
+  target?: string
   nutriotionalGoals?: NutritionGoals[]
 }
