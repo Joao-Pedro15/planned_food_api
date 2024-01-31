95bcc845dc48f92beeb836b0119b1a624464b62e
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-31 08:34:50 -0300

    feat: create file test by usecase calc base metabolic rate

---
+++ src/usecases/baseMetabolicRate/BaseMetabolicRate.spec.ts
@@ -0,0 +1,23 @@
+import { BaseMetabolicRate } from "./BaseMetabolicRate"
+
+describe('testing usecase metabolic', () => {
+
+ describe('men cases calc metabolic', () => {
+
+  const baseProps = {
+   age: 31,
+   gender: "male" as any,
+   height: 179,
+   weight: 90
+  }
+
+  it('should keep target metabolic rate and activity medium', () => {
+   const base = new BaseMetabolicRate({ ...baseProps, activity: 'mediumActivity' })
+   const result = base.calc('keep')
+   expect(result).toEqual('0')
+
+  })
+
+ })
+
+})
