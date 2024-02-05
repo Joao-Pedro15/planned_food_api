bb5df3214b1c4815b8b81efde97d4d1be5519c56
joao pedro <jjoao.monteiro15@gmail.com>
2024-02-04 21:20:10 -0300

    fix: add gender in test user

--- src/domain/user/User.spec.ts
+++ src/domain/user/User.spec.ts
@@ -5,6 +5,6 @@ describe('testing user domain', () => {
   it('should return instance User', () => {
     const userDto: UserDTO = { email: 'fakeEmail', name: 'fakeName', password: 'fakePassword', age: 21, gender: 'male', height: 180, weight: 85 }
     const user = new User(userDto)
-    expect(Object.keys(user)).toEqual(['id', 'name', 'email', 'password', 'age', 'height', 'weight', 'nutritionalGoals'])
+    expect(Object.keys(user)).toEqual(['id', 'name', 'email', 'password', 'age', 'height', 'gender', 'weight', 'nutritionalGoals'])
   })
 })
